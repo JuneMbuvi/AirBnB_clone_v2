3a88980edaced2807b445f465c234eb88d42ea80
JuneMbuvi <mbuvimbatha@gmail.com>
2023-12-07 05:44:39 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-#script that sets up your web servers for the deployment of web_static
+#a Bash script that sets up your web servers for the deployment of web_static
 
+#Install Nginx if it not already installed
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
+#sudo ufw allow 'HTTP nginx'
 
+#creating folders if they don't exist
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
@@ -11,15 +14,16 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
-<head>
-</head>
-<body>
-Holberton School
-</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/index.html /data/web_static/current
+#a symbolic link /data/web_static/current linked to the test folder
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
-sudo sed -i 'listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 service nginx restart
